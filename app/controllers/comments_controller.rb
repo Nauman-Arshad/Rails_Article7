3e738d2540559da1844c9ac79f66dc12fa4b3c9e
Nauman-Arshad <naumankhan642@gmail.com>
2021-12-30 15:24:44 +0500

    add admin authentication

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+  http_basic_authenticate_with name: "admin", password: "admin", except: [:index, :show]
+
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
