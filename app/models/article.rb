c8c33c4fbefcd5728eb17a8d44cd1f6dc533ae2b
Nauman-Arshad <naumankhan642@gmail.com>
2021-12-30 15:17:41 +0500

    add status

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,8 @@
 class Article < ApplicationRecord
-  has_many :comments
+  include Visible
+
+  has_many :comments, dependent: :destroy
+
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
 end
